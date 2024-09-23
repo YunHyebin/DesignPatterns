fabb007e76eab4c48d5850a49ae0d92665eff986
YunHyebin <qpqpqp17820@gmail.com>
2024-09-23 16:05:10 +0900

    behavioral: visitor pattern

---
+++ src/behavioralPattern/visitor/Circle.java
@@ -0,0 +1,20 @@
+package behavioralPattern.visitor;
+
+public class Circle implements Shape {
+	//ConcreteElement: Circle
+	private double radius;
+	
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
+
+	@Override
+	public void accept(Visitor visitor) {
+		visitor.visit(this);
+	}
+
+}
